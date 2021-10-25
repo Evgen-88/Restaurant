e1f30f7abfd045b551679c1bed4b083f797b83f5
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-10-25 09:23:40 +0300

    updating runner class and gitignore

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -1,7 +1,5 @@
 package com.itrex.kaliaha;
 
-import com.itrex.kaliaha.repository.OrderRepository;
-import com.itrex.kaliaha.repository.impl.JDBCOrderRepositoryImpl;
 import com.itrex.kaliaha.service.FlywayService;
 import org.h2.jdbcx.JdbcConnectionPool;
 
@@ -11,10 +9,7 @@ public class Runner {
     public static void main(String[] args) {
         FlywayService flywayService = new FlywayService();
         flywayService.migrate();
-
         JdbcConnectionPool jdbcConnectionPool = JdbcConnectionPool.create(H2_URL, H2_USER, H2_PASSWORD);
-
         jdbcConnectionPool.dispose();
-
     }
 }
