9349dff2d566187c4d3b02e33355b4a48b6dc015
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-10-25 23:35:57 +0300

    removing unusful data

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -3,7 +3,9 @@ package com.itrex.kaliaha;
 import com.itrex.kaliaha.service.FlywayService;
 import org.h2.jdbcx.JdbcConnectionPool;
 
-import static com.itrex.kaliaha.property.Properties.*;
+import static com.itrex.kaliaha.property.Properties.H2_URL;
+import static com.itrex.kaliaha.property.Properties.H2_USER;
+import static com.itrex.kaliaha.property.Properties.H2_PASSWORD;
 
 public class Runner {
     public static void main(String[] args) {
