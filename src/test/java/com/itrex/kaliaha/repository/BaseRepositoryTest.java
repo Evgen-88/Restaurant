d255d1de012053a33f4fcc5918407e32c2585e45
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-10-26 16:47:53 +0300

    Final Refactoring

--- src/test/java/com/itrex/kaliaha/repository/BaseRepositoryTest.java
+++ src/test/java/com/itrex/kaliaha/repository/BaseRepositoryTest.java
@@ -10,14 +10,12 @@ import static com.itrex.kaliaha.property.Properties.H2_USER;
 import static com.itrex.kaliaha.property.Properties.H2_PASSWORD;
 
 public abstract class BaseRepositoryTest {
-
     private final FlywayService flywayService;
     private final JdbcConnectionPool connectionPool;
 
     public BaseRepositoryTest() {
         flywayService = new FlywayService();
         connectionPool = JdbcConnectionPool.create(H2_URL, H2_USER, H2_PASSWORD);
-        flywayService.clean();
     }
 
     @Before
