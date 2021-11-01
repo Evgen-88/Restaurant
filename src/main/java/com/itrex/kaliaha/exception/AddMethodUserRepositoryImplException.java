a9c73331f8ea164d55761fdcee6a5429ad3486d4
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-01 11:57:46 +0300

    creating exception for user repository

---
+++ src/main/java/com/itrex/kaliaha/exception/AddMethodUserRepositoryImplException.java
@@ -0,0 +1,18 @@
+package com.itrex.kaliaha.exception;
+
+public class AddMethodUserRepositoryImplException extends RuntimeException{
+    public AddMethodUserRepositoryImplException() {
+    }
+
+    public AddMethodUserRepositoryImplException(String message) {
+        super(message);
+    }
+
+    public AddMethodUserRepositoryImplException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AddMethodUserRepositoryImplException(Throwable cause) {
+        super(cause);
+    }
+}
