b141ddb71d97af5f007a1e9437dd986a9ccf79f2
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-02 16:08:41 +0300

    refactoring runner

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -1,17 +1,14 @@
 package com.itrex.kaliaha;
 
+import com.itrex.kaliaha.config.ApplicationContextConfiguration;
 import com.itrex.kaliaha.service.FlywayService;
-import org.h2.jdbcx.JdbcConnectionPool;
-
-import static com.itrex.kaliaha.property.Properties.H2_URL;
-import static com.itrex.kaliaha.property.Properties.H2_USER;
-import static com.itrex.kaliaha.property.Properties.H2_PASSWORD;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Runner {
     public static void main(String[] args) {
-        FlywayService flywayService = new FlywayService();
-        flywayService.migrate();
-        JdbcConnectionPool jdbcConnectionPool = JdbcConnectionPool.create(H2_URL, H2_USER, H2_PASSWORD);
-        jdbcConnectionPool.dispose();
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
+        FlywayService flywayService = ctx.getBean(FlywayService.class);
+        flywayService.clean();
     }
 }
