29ee3a41e234e0641686eafaa053f47d89cfb3a8
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-02 16:02:26 +0300

    refactoring to thread safe class

--- src/main/java/com/itrex/kaliaha/util/HibernateUtil.java
+++ src/main/java/com/itrex/kaliaha/util/HibernateUtil.java
@@ -1,20 +1,16 @@
 package com.itrex.kaliaha.util;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory;
+    private HibernateUtil() {}
+
+    private static class SessionFactoryHolder {
+        public static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                sessionFactory = new Configuration().configure().buildSessionFactory();
-            } catch (HibernateException e) {
-                System.err.println("Failed to create sessionFactory object." + e);
-            }
-        }
-        return sessionFactory;
+        return SessionFactoryHolder.sessionFactory;
     }
 }
