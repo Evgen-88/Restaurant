33ff93ceeee644ef95e517ced425a3ab8425956e
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-03 22:49:42 +0300

    refactoring delete irrelevant rows

--- src/main/java/com/itrex/kaliaha/config/ApplicationContextConfiguration.java
+++ src/main/java/com/itrex/kaliaha/config/ApplicationContextConfiguration.java
@@ -1,7 +1,7 @@
 package com.itrex.kaliaha.config;
 
 import org.flywaydb.core.Flyway;
-import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
