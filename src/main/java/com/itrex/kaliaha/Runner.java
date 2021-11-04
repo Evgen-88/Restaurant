653cd91467e33fb7c8a20293a340602019c2f1a9
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-04 15:14:52 +0300

    checking work with mysql db

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -1,14 +1,13 @@
 package com.itrex.kaliaha;
 
-import com.itrex.kaliaha.config.ApplicationContextConfiguration;
-import org.flywaydb.core.Flyway;
+import com.itrex.kaliaha.config.MigrationContextConfiguration;
+import com.itrex.kaliaha.repository.UserRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Runner {
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
-        Flyway flyway = ctx.getBean(Flyway.class);
-        flyway.clean();
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(MigrationContextConfiguration.class);
+        ctx.getBean(UserRepository.class).findAll().forEach(System.out::println);
     }
 }
