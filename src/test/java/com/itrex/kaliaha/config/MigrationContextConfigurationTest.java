38d91f6fc6524f746e777322d6ebcfd885c32fc5
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-04 15:13:12 +0300

    split java-config into main project and tests

---
+++ src/test/java/com/itrex/kaliaha/config/MigrationContextConfigurationTest.java
@@ -0,0 +1,36 @@
+package com.itrex.kaliaha.config;
+
+import org.flywaydb.core.Flyway;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/db_test.properties")
+@ComponentScan("com.itrex.kaliaha.repository")
+public class MigrationContextConfigurationTest {
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.user}")
+    private String user;
+    @Value("${database.password}")
+    private String password;
+    @Value("${database.migration.location}")
+    private String migrationLocation;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return Flyway.configure()
+                .dataSource(url, user, password)
+                .locations(migrationLocation)
+                .load();
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+    }
+}
