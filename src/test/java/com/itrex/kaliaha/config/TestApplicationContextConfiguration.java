54021fecae31141244aa74be15d904de600eb17f
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-08 14:00:36 +0300

    renaming test configuration file

--- src/test/java/com/itrex/kaliaha/config/TestApplicationContextConfiguration.java
+++ src/test/java/com/itrex/kaliaha/config/TestApplicationContextConfiguration.java
@@ -1,14 +1,17 @@
 package com.itrex.kaliaha.config;
 
 import org.flywaydb.core.Flyway;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:/db_test.properties")
-public class MigrationContextConfigurationTest {
+@ComponentScan("com.itrex.kaliaha")
+@PropertySource("classpath:/db.properties")
+public class TestApplicationContextConfiguration {
     @Value("${database.url}")
     private String url;
     @Value("${database.user}")
@@ -24,5 +27,10 @@ public class MigrationContextConfigurationTest {
                 .dataSource(url, user, password)
                 .locations(migrationLocation)
                 .load();
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
     }
 }
