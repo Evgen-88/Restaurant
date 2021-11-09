516757882e1d0311356ec829d2e5c92a888247e5
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-09 23:52:32 +0300

    refactoring runner

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -1,15 +1,25 @@
 package com.itrex.kaliaha;
 
 import com.itrex.kaliaha.config.ApplicationContextConfiguration;
+import com.itrex.kaliaha.dto.DishUpdateDTO;
+import com.itrex.kaliaha.enums.DishGroup;
+import com.itrex.kaliaha.exception.ServiceException;
 import com.itrex.kaliaha.service.DishService;
-import com.itrex.kaliaha.service.impl.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ServiceException {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
         DishService dishService = ctx.getBean(DishService.class);
-        dishService.findById(1L).getIngredientList().forEach(System.out::println);
+//        DishUpdateDTO dishUpdateDTO =  DishUpdateDTO.builder()
+//                .id(1L).dishGroup(DishGroup.SALAD).dishName("фирменное блюдо")
+//                .dishDescription("delicious").price(5400).imagePath("asd1")
+//                .build();
+        DishUpdateDTO dishUpdateDTO =  DishUpdateDTO.builder()
+                .id(1L).dishGroup(DishGroup.HOT).dishName("update")
+                .dishDescription("der").price(10).imagePath("s")
+                .build();
+        dishService.update(dishUpdateDTO);
     }
 }
