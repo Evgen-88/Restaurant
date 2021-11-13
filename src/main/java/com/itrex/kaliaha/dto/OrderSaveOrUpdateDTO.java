8c30bfaf7ade40885e96f8fb91ba1f0c9654937b
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-13 03:06:54 +0300

    replace the field user to userId

---
+++ src/main/java/com/itrex/kaliaha/dto/OrderSaveOrUpdateDTO.java
@@ -0,0 +1,29 @@
+package com.itrex.kaliaha.dto;
+
+import com.itrex.kaliaha.enums.OrderStatus;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+public class OrderSaveOrUpdateDTO implements DTO{
+    private Long orderId;
+    private int price;
+    private LocalDate date;
+    private String address;
+    private OrderStatus orderStatus;
+    private Long userId;
+}
