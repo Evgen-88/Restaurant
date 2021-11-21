06245fa3e8d267e8b444f0017a4e3de7299bf58c
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-21 22:08:40 +0300

    refactoring configuration files

--- src/test/java/com/itrex/kaliaha/config/TestServiceConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.itrex.kaliaha.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.itrex.kaliaha.service")
-public class TestServiceConfiguration {}
