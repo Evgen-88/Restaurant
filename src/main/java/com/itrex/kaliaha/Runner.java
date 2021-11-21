b3bec0a2f6efa54a492887bbcc28999dada30bfc
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-22 00:25:23 +0300

    creating runner for spring boot

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -1,23 +1,11 @@
 package com.itrex.kaliaha;
 
-import com.itrex.kaliaha.config.ApplicationContextConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.itrex.kaliaha.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.AbstractEnvironment;
-
-@Slf4j
+@SpringBootApplication
 public class Runner {
     public static void main(String[] args) {
-        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "info");
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
-
-        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
-        System.out.print(userRepository.findById(1L));
-
-        log.info("info");
-        log.debug("debug");
+        SpringApplication.run(Runner.class, args);
     }
 }
