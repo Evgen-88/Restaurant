ebf79d4be6f73d04aff099d7c6bc107c7b7f1ce7
KaliahaVladzislav <kaliaha.vladzislav@mail.ru>
2021-11-23 19:22:20 +0300

    refactoring runner

--- src/main/java/com/itrex/kaliaha/Runner.java
+++ src/main/java/com/itrex/kaliaha/Runner.java
@@ -1,11 +1,26 @@
 package com.itrex.kaliaha;
 
+import com.itrex.kaliaha.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @SpringBootApplication
-public class Runner {
+public class Runner implements CommandLineRunner {
+    @Autowired
+    ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
+        userRepository.findById(1L);
     }
 }
